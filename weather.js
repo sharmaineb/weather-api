8b939f41e002ca0949b17395283aaabe3117d765
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-09-09 16:36:02 -0700

    [update] lab completed.

--- weather.js
+++ weather.js
@@ -1,42 +1,57 @@
-async function getWeather(url) {
-  const res = await fetch(url) 
-  const json = await res.json() 
-  console.log(json)
-  const WeatherJson = {
-      'name': json.name,
-      'desc': json.weather[0].description,
-      'temp': json.main.temp,
-      'coordLat': json.coord.lat,
-      'coordLon': json.coord.lon,
-      'humidity': json.main.humidity,
-      'pressure': json.main.pressure,
-      'tempMax': json.main.temp_max,
-      'tempMin': json.main.temp_min
+async function fetchWeatherData(url) {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error('Sorry, Failed to fetch data');
+    }
+
+    return await response.json();
+  } catch (error) {
+    throw new Error('Sorry, Failed to fetch weather data');
   }
-  return WeatherJson
 }
 
-async function getWeatherZip(apiKey, zip, units='imperial') {
-  const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&${units}`
-  return getWeather(url)
+async function getWeatherZip(apiKey, zip, units = 'imperial') {
+  const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`;
+  return getWeather(apiKey, url);
 }
 
-async function getWeatherCity(apiKey, city) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-  return getWeather(url)
+async function getWeatherCity(apiKey, city, units = 'imperial') {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  return getWeather(apiKey, url);
 }
 
-async function getWeatherGeo(apiKey, lat, lon, units='imperial') {
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&${units}`
-  return getWeather(url)
+async function getWeatherGeo(apiKey, lat, lon, units = 'imperial') {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+  return getWeather(apiKey, url);
 }
 
+async function getWeather(apiKey, url) {
+  try {
+    const data = await fetchWeatherData(url);
 
-export {
-  getWeatherZip,
-  getWeatherCity,
-  getWeatherGeo
+    return {
+      name: data.name,
+      description: data.weather[0].description,
+      temperature: data.main.temp,
+      humidity: data.main.humidity,
+      pressure: data.main.pressure,
+      tempMax: data.main.temp_max,
+      tempMin: data.main.temp_min,
+      coordLat: data.coord.lat,
+      coordLon: data.coord.lon,
+    };
+  } catch (error) {
+    throw new Error('Sorry, Failed to get weather information');
+  }
 }
+
+export { 
+  getWeatherZip, 
+  getWeatherCity, 
+  getWeatherGeo 
+};
 
 
 
